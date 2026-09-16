4fabb21f6a66f9511f558f71655d28a903ed53b4
mihchis <trin79136@gmail.com>
2026-09-16 18:53:07 +0700

    fix: perfectly center actress and movie dialogs in viewport

--- client/components/ui/dialog.tsx
+++ client/components/ui/dialog.tsx
@@ -31,7 +31,7 @@ function DialogOverlay({
     <DialogPrimitive.Backdrop
       data-slot="dialog-overlay"
       className={cn(
-        'data-closed:animate-out data-closed:fade-out-0 data-open:animate-in data-open:fade-in-0 fixed inset-0 isolate z-50 bg-black/10 duration-100 supports-backdrop-filter:backdrop-blur-xs',
+        'data-closed:animate-out data-closed:fade-out-0 data-open:animate-in data-open:fade-in-0 fixed inset-0 isolate z-50 bg-black/70 duration-100 backdrop-blur-sm',
         className,
       )}
       {...props}
